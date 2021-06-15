80cfbb3d7f7373c5b5eb248b899233598d5b5fa3
Brian Cartan <cartanb@gmail.com>
2021-06-15 17:01:15 -0400

    Created welcome page and product page placeholders

---
+++ client/components/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to the Grand-Shoppe!</h1>
+      <p>
+        Need a hug? A cookie? A bedtime story? Our trained professionals are
+        full of Big Grandparent Energy and ready to work with you to fulfill
+        your grandparenting needs!
+      </p>
+      <h2>Call today!</h2>
+    </div>
+  );
+};
+
+export default Welcome;
