a3243e2349141f22a0e782987675ff7fa1c0afd1
Brian Cartan <cartanb@gmail.com>
2021-06-15 12:20:07 -0400

    Refactored redux store with Redux Toolkit

--- client/store/auth.js
+++ client/store/auth.js
@@ -1,60 +1,96 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import history from '../history';
 
-const TOKEN = 'token'
+const TOKEN = 'token';
 
 /**
  * ACTION TYPES
  */
-const SET_AUTH = 'SET_AUTH'
+const SET_AUTH = 'SET_AUTH';
 
 /**
  * ACTION CREATORS
  */
-const setAuth = auth => ({type: SET_AUTH, auth})
+const setAuth = (auth) => ({ type: SET_AUTH, auth });
 
 /**
  * THUNK CREATORS
  */
-export const me = () => async dispatch => {
-  const token = window.localStorage.getItem(TOKEN)
-  if (token) {
-    const res = await axios.get('/auth/me', {
-      headers: {
-        authorization: token
-      }
-    })
-    return dispatch(setAuth(res.data))
-  }
-}
+// export const me = () => async (dispatch) => {
+//   const token = window.localStorage.getItem(TOKEN);
+//   if (token) {
+//     const res = await axios.get('/auth/me', {
+//       headers: {
+//         authorization: token,
+//       },
+//     });
+//     return dispatch(setAuth(res.data));
+//   }
+// };
 
-export const authenticate = (username, password, method) => async dispatch => {
+export const me = createAsyncThunk('auth/me', async (dispatch) => {
   try {
-    const res = await axios.post(`/auth/${method}`, {username, password})
-    window.localStorage.setItem(TOKEN, res.data.token)
-    dispatch(me())
-  } catch (authError) {
-    return dispatch(setAuth({error: authError}))
+    const token = window.localStorage.getItem(TOKEN);
+    if (token) {
+      const res = await axios.get('/auth/me', {
+        headers: {
+          authorization: token,
+        },
+      });
+      return res.data;
+    }
+  } catch (error) {
+    console.log(error);
   }
-}
+});
+
+export const authenticate =
+  (username, password, method) => async (dispatch) => {
+    try {
+      const res = await axios.post(`/auth/${method}`, { username, password });
+      window.localStorage.setItem(TOKEN, res.data.token);
+      dispatch(me());
+    } catch (authError) {
+      return dispatch(setAuth({ error: authError }));
+    }
+  };
 
 export const logout = () => {
-  window.localStorage.removeItem(TOKEN)
-  history.push('/login')
+  window.localStorage.removeItem(TOKEN);
+  history.push('/login');
   return {
     type: SET_AUTH,
-    auth: {}
-  }
-}
+    auth: {},
+  };
+};
 
 /**
  * REDUCER
  */
-export default function(state = {}, action) {
-  switch (action.type) {
-    case SET_AUTH:
-      return action.auth
-    default:
-      return state
-  }
-}
+// export default function (state = {}, action) {
+//   switch (action.type) {
+//     case SET_AUTH:
+//       return action.auth;
+//     default:
+//       return state;
+//   }
+// }
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {},
+  reducers: {
+    auth: (state, action) => {
+      state = action.auth;
+    },
+  },
+  extraReducers: {
+    [me.fulfilled]: (state, action) => {
+      state = action.payload;
+    },
+  },
+});
+
+export const { auth } = authSlice.actions;
+export default authSlice.reducer;
--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,19 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import auth from './auth'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { configureStore } from '@reduxjs/toolkit';
+import auth from './auth';
 
-const reducer = combineReducers({ auth })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+// const reducer = combineReducers({ auth });
+// const middleware = composeWithDevTools(
+//   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+// );
+// const store = createStore(reducer, middleware);
 
-export default store
-export * from './auth'
+export default configureStore({
+  reducer: { auth },
+  middleware: [thunkMiddleware, createLogger({ collapsed: true })],
+  devTools: true,
+});
+export * from './auth';
