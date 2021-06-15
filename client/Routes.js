1e7cb975b8863856d1a4fc217d16427c9e07449a
Brian Cartan <cartanb@gmail.com>
2021-06-15 17:23:09 -0400

    Refactored Routes component

--- client/Routes.js
+++ client/Routes.js
@@ -1,5 +1,5 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
@@ -12,55 +12,39 @@ import { me } from './store';
 /**
  * COMPONENT
  */
-class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData();
-  }
+const Routes = () => {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => !!state.auth.id);
 
-  render() {
-    const { isLoggedIn } = this.props;
-
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/" exact component={Welcome} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/products/:id" component={SingleProduct} />
-            <Route path="/products" component={AllProducts} />
-            <Route path="/cart" component={Cart} />
-          </Switch>
-        )}
-      </div>
-    );
-  }
-}
-
-/**
- * CONTAINER
- */
-const mapState = (state) => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.auth that has a truthy id.
-    // Otherwise, state.auth will be an empty object, and state.auth.id will be falsey
-    isLoggedIn: !!state.auth.id,
+  const loadInitialData = () => {
+    dispatch(me);
   };
-};
 
-const mapDispatch = (dispatch) => {
-  return {
-    loadInitialData() {
-      dispatch(me());
-    },
-  };
+  useEffect(() => {
+    loadInitialData();
+  }, []);
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Redirect to="/home" />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route path="/" exact component={Welcome} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/products/:id" component={SingleProduct} />
+          <Route path="/products" component={AllProducts} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+      )}
+    </div>
+  );
 };
 
 // The `withRouter` wrapper makes sure that updates are not blocked
 // when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes));
+export default withRouter(Routes);
