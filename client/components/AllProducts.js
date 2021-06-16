c1e1e9f8c9b10da0d590596b37ff3b81ff97297c
Sam Hannan <samhannan47@gmail.com>
2021-06-16 14:49:55 -0400

    All Products Vertical Slice

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,15 +1,34 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadAllProducts } from '../store/allProducts';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadAllProducts } from "../store/allProducts";
+import { Link } from "react-router-dom";
 
 const AllProducts = () => {
+  const allProducts = useSelector((state) => state.allProducts);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadAllProducts());
   }, []);
 
-  return <div>ALL PRODUCTS PLACEHOLDER</div>;
+  return (
+    <div>
+      <ul>
+        {allProducts.map((product) => {
+          return (
+            <li key={product.id}>
+              <Link to={`/products/${product.id}`}>
+                <img src={product.imageUrl} />
+                <p>{product.name}</p>
+              </Link>
+              <p>${product.cost}/day</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default AllProducts;
