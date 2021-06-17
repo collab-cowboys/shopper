c8e5076da0d5855ccd8ecd8d90b32cf6b909a54e
Brian Cartan <cartanb@gmail.com>
2021-06-17 15:43:45 -0400

    Refactored navbar to include all-products and (unlinked) cart

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout } from "../store";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout } from '../store';
 
 const Navbar = () => {
   const isLoggedIn = useSelector((state) => !!state.auth.id);
@@ -26,6 +26,8 @@ const Navbar = () => {
             {/* The navbar will show these links before you log in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <Link to="/products">All Grandparents</Link>
+            <span>My Cart</span>
           </div>
         )}
       </nav>
