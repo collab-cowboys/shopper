32e27b9ecd6ff9ef64469a381bd681af46b383aa
Brian Cartan <cartanb@gmail.com>
2021-06-17 12:21:54 -0400

    Added Transaction model to db

---
+++ server/db/models/transaction.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+const axios = require('axios');
+
+const Transaction = db.define('transaction', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 1,
+    },
+  },
+  totalPrice: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 1,
+    },
+  },
+});
+module.exports = Transaction;
+/**
+ * instanceMethods
+ */
+// no instance methods for now, but we will add them here if we need any
+/**
+ * classMethods
+ */
+// no class methods for now, but we will add them here if we need any
+/**
+ * hooks
+ */
+// no class methods for now, but we will add them here if we need any
