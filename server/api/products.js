142e4424301aaddb893741585f2bb5480a9327c1
Sam Hannan <samhannan47@gmail.com>
2021-06-21 17:28:23 -0400

    working delete rout for ADMIN ONLY

--- server/api/products.js
+++ server/api/products.js
@@ -1,13 +1,14 @@
-const router = require('express').Router();
-const Product = require('../db/models/product');
+const router = require("express").Router();
+const Product = require("../db/models/product");
+const { adminKeeper } = require("./gatekeeper");
 
 // GET /api/products
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     res.send(
       await Product.findAll({
-        order: [['id', 'ASC']],
+        order: [["id", "ASC"]],
       })
     );
   } catch (error) {
@@ -17,12 +18,22 @@ router.get('/', async (req, res, next) => {
 
 // GET /api/products/:id
 
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.id);
     res.json(product);
   } catch (error) {
     next(error);
+  }
+});
+
+router.delete("/:id", adminKeeper, async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    await product.destroy();
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
   }
 });
 
