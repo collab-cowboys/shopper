67fd5ad9e61187bcda56fad237031d9a0c111636
Drew LaFiandra <79876588+alafiand@users.noreply.github.com>
2021-06-21 18:47:43 -0700

    protected signup from injections

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,33 +1,36 @@
-const router = require('express').Router()
-const { models: {User }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { User },
+} = require("../db");
+module.exports = router;
 
-router.post('/login', async (req, res, next) => {
+router.post("/login", async (req, res, next) => {
   try {
-    res.send({ token: await User.authenticate(req.body)}); 
+    res.send({ token: await User.authenticate(req.body) });
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-
-router.post('/signup', async (req, res, next) => {
+router.post("/signup", async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
-    res.send({token: await user.generateToken()})
+    //destructure necessary info from request to prevent injection attacks
+    const { username, email, password } = req.body;
+    const user = await User.create({ username, email, password });
+    res.send({ token: await user.generateToken() });
   } catch (err) {
-    if (err.name === 'SequelizeUniqueConstraintError') {
-      res.status(401).send('User already exists')
+    if (err.name === "SequelizeUniqueConstraintError") {
+      res.status(401).send("User already exists");
     } else {
-      next(err)
+      next(err);
     }
   }
-})
+});
 
-router.get('/me', async (req, res, next) => {
+router.get("/me", async (req, res, next) => {
   try {
-    res.send(await User.findByToken(req.headers.authorization))
+    res.send(await User.findByToken(req.headers.authorization));
   } catch (ex) {
-    next(ex)
+    next(ex);
   }
-})
+});
