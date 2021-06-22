04f46fab14d8c751211cef196657743cccc2441b
Brian Cartan <cartanb@gmail.com>
2021-06-22 12:59:28 -0400

    Cart component displays message instead of blank table if no items in cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { getCartProducts } from '../store/cart';
 import CartEntry from './CartEntry';
 
@@ -11,32 +12,45 @@ const Cart = () => {
     dispatch(getCartProducts());
   }, []);
 
+  const transactions = Object.keys(cart);
+
   return (
     <div>
-      <table id="cart">
-        <tbody>
-          <tr>
-            <th>Product Info</th>
-            <th>Quantity</th>
-            <th>Remove?</th>
-            <th>Total Price</th>
-          </tr>
-          {Object.keys(cart).map((transaction) => {
-            const { id, name, imageUrl } = cart[transaction].product;
-            return (
-              <CartEntry
-                key={id}
-                name={name}
-                imageUrl={imageUrl}
-                quantity={cart[transaction].quantity}
-                totalPrice={cart[transaction].totalPrice}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-      <button type="button">Sign Up &amp; Checkout</button>
-      <button type="button">Checkout as Guest</button>
+      {transactions.length ? (
+        <div>
+          <table id="cart">
+            <tbody>
+              <tr>
+                <th>Product Info</th>
+                <th>Quantity</th>
+                <th>Remove?</th>
+                <th>Total Price</th>
+              </tr>
+              {transactions.map((transaction) => {
+                const { id, name, imageUrl } = cart[transaction].product;
+                return (
+                  <CartEntry
+                    key={id}
+                    name={name}
+                    imageUrl={imageUrl}
+                    quantity={cart[transaction].quantity}
+                    totalPrice={cart[transaction].totalPrice}
+                  />
+                );
+              })}
+            </tbody>
+          </table>
+          <button type="button">Sign Up &amp; Checkout</button>
+          <button type="button">Checkout as Guest</button>
+        </div>
+      ) : (
+        <div>
+          <p>
+            You have no items in your cart! Why not look at some of our{' '}
+            <Link to="/products">product options</Link>?
+          </p>
+        </div>
+      )}
     </div>
   );
 };
