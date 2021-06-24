738401d1e2fa837830daaac840b741b6c6e8313a
Jared Irwin <jaredtirwin@gmail.com>
2021-06-23 22:11:54 -0400

    added roles instead of gender to SingleProuct component

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { getSingleProduct } from '../store/singleProduct';
-import AddToCart from './AddToCart';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { getSingleProduct } from "../store/singleProduct";
+import AddToCart from "./AddToCart";
 
 const SingleProduct = (props) => {
   const product = useSelector((state) => state.singleProduct);
-  let { name, age, gender, skills, cost, imageUrl, inStock } = product;
+  let { name, age, skills, cost, imageUrl, inStock } = product;
   skills = skills || [];
   const dispatch = useDispatch();
   const { id } = useParams();
@@ -21,9 +21,24 @@ const SingleProduct = (props) => {
         <img src={imageUrl} alt="photo" />
         <p>The {name}</p>
         <p>
-          {age}, {gender}
+          age: {age}
+          <form>
+            <div>
+              <label>Role:</label>
+              <select name="role" id="role">
+                <option value="grandpa">Grandpa</option>
+                <option value="grandma">Grandma</option>
+                <option value="great uncle">Great Uncle</option>
+                <option value="great aunt">Great Aunt</option>
+                <option value="abuelo">Abuelo</option>
+                <option value="abuela">Abuela</option>
+                <option value="Tío abuelo">Tío abuelo</option>
+                <option value="Tía abuela">Tía abuela</option>
+              </select>
+            </div>
+          </form>
         </p>
-        <p>Special skills: {skills.map((skill) => skill).join(', ')}</p>
+        <p>Special skills: {skills.map((skill) => skill).join(", ")}</p>
         <p>${cost}</p>
         <p>{inStock} left in stock</p>
         <AddToCart product={product} />
